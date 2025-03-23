88f92a2e7ffa0fb5f9bf894e7d7ddda1fa9c5eae
AWhite-Code <Alexwhitehead432@gmail.com>
2025-03-23 15:03:59 +0000

    Readability refactor of ingredients seeding so it can now be called by calling ingredientsbyname.ITEMNAME

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,53 +2,37 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    // Clear out old content - dont think I need it but this is good practice lol
+    // Clear out old content - don't think I need it but this is good practice lol
     await prisma.recipeIngredient.deleteMany({})
     await prisma.recipe.deleteMany({})
     await prisma.ingredient.deleteMany({})
 
     // Adding basic ingredients
-    const ingredients = await Promise.all([
-        prisma.ingredient.create({
-            data: { name: 'Onion' },
-        }),
+    const ingredientsArray = await Promise.all([
+        prisma.ingredient.create({data: { name: 'Onion' }}),
+        prisma.ingredient.create({data: { name: 'Garlic' }}),
+        prisma.ingredient.create({data: { name: 'Beef Mince' }}),
+        prisma.ingredient.create({data: { name: 'Lardons' }}),
+        prisma.ingredient.create({data: { name: 'Sweetcorn' }}),
+        prisma.ingredient.create({data: { name: 'Smoked Paprika' }}),
+        prisma.ingredient.create({data: { name: 'Cayenne Pepper' }}),
+        prisma.ingredient.create({data: { name: 'Cumin' }}),
+        prisma.ingredient.create({data: { name: 'Red Pepper' }}),
+        prisma.ingredient.create({data: { name: 'Beef Stock' }})
+    ]);
 
-        prisma.ingredient.create({
-            data: { name: 'Garlic' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Beef Mince' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Lardons' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Sweetcorn' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Smoked Paprika' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Cayenne Pepper' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Cumin' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Red Pepper' }
-        }),
-
-        prisma.ingredient.create({
-            data: { name: 'Beef Stock' }
-        })
-    ])
+    const ingredientsByName = {
+        onion: ingredientsArray[0],
+        garlic: ingredientsArray[1],
+        beefMince: ingredientsArray[2],
+        lardons: ingredientsArray[3],
+        sweetCorn: ingredientsArray[4],
+        smokedPaprika: ingredientsArray[5],
+        cayennePepper: ingredientsArray[6],
+        cumin: ingredientsArray[7],
+        redPepper: ingredientsArray[8],
+        beefStock: ingredientsArray[9]
+    }
 }
 
 
