57350ae2c9d6c72b51325f3f8c7c1306acbea572
AWhite-Code <Alexwhitehead432@gmail.com>
2025-04-05 01:01:42 +0100

    Moved jest.config into its own folder for my own sanity

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,10 +1,9 @@
-// Set the environment to test before any code runs for client.ts configuration
 process.env.NODE_ENV = 'test';
 
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    setupFilesAfterEnv: ['./jest-setup.ts'],
+    setupFilesAfterEnv: ['./config/jest-setup.ts'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
